5c147e5a21f9305eb33d85d85b8e9df133ebdcf4
y1shiny1shin <y1shin@163.com>
2024-08-16 14:43:45 +0800

    测试推送

--- src/main/java/com/test.java
+++ src/main/java/com/test.java
@@ -6,5 +6,6 @@ import java.lang.reflect.InvocationTargetException;
 public class test {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException {
         new ProcessBuilder("whoami").start().getInputStream();
+        new ProcessBuilder("测试消息").start().getInputStream();
     }
 }
